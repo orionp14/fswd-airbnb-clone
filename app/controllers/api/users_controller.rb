288b9038652c040bb8176e60f30d015f37f8af52
h8rry <harrychenca@gmail.com>
2023-08-02 09:29:45 +0200

    Fixes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ module Api
     def create
       @user = User.new(user_params)
 
-      if @user.save
+      if @user.save!
         render 'api/users/create', status: :created
       else
         render json: { success: false }, status: :bad_request
