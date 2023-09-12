24b26009f2860531acf1a929068ff45d671da4a8
h8rry <harrychenca@gmail.com>
2023-09-12 13:07:09 +0800

    Hide edit property for non-owners

--- app/javascript/src/property/property.jsx
+++ app/javascript/src/property/property.jsx
@@ -140,126 +140,126 @@ class Property extends React.Component {
 // Create a separate EditForm component
 function EditForm({ property, onInputChange, onSubmit, onCancel }) {
   return (
-<div className="container edit-form">
-  <h3>Edit Property</h3>
-  <form onSubmit={onSubmit}>
-    <div className="form-group mb-2">
-      <label htmlFor="title">Title</label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        value={property.title}
-        onChange={onInputChange}
-        className="form-control"
-      />
-    </div>
-    <div className="form-group mb-2">
-      <label htmlFor="description">Description</label>
-      <textarea
-        id="description"
-        name="description"
-        value={property.description}
-        onChange={onInputChange}
-        className="form-control"
-      />
-    </div>
-    <div className="row">
-    <div className="col-md-6">
-    <div className="form-group mb-2">
-      <label htmlFor="property_type">Property Type</label>
-      <input
-        type="text"
-        id="property_type"
-        name="property_type"
-        value={property.property_type}
-        onChange={onInputChange}
-        className="form-control"
-      />
-    </div>
-    </div>
-      <div className="col-md-6">
+    <div className="container edit-form">
+      <h3>Edit Property</h3>
+      <form onSubmit={onSubmit}>
         <div className="form-group mb-2">
-          <label htmlFor="max_guests">Max Guests</label>
+          <label htmlFor="title">Title</label>
           <input
-            type="number"
-            id="max_guests"
-            name="max_guests"
-            value={property.max_guests}
+            type="text"
+            id="title"
+            name="title"
+            value={property.title}
             onChange={onInputChange}
             className="form-control"
           />
         </div>
-      </div>
-      </div>
-      <div className="row">
-      <div className="col-md-6">
         <div className="form-group mb-2">
-          <label htmlFor="price_per_night">Price Per Night</label>
-          <input
-            type="number"
-            id="price_per_night"
-            name="price_per_night"
-            value={property.price_per_night}
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            value={property.description}
             onChange={onInputChange}
             className="form-control"
           />
         </div>
-      </div>
-      <div className="col-md-6">
+        <div className="row">
+        <div className="col-md-6">
         <div className="form-group mb-2">
-          <label htmlFor="bedrooms">Bedrooms</label>
+          <label htmlFor="property_type">Property Type</label>
           <input
-            type="number"
-            id="bedrooms"
-            name="bedrooms"
-            value={property.bedrooms}
+            type="text"
+            id="property_type"
+            name="property_type"
+            value={property.property_type}
             onChange={onInputChange}
             className="form-control"
           />
         </div>
-      </div>
-      </div>
-      <div className="row">
-      <div className="col-md-6">
+        </div>
+          <div className="col-md-6">
+            <div className="form-group mb-2">
+              <label htmlFor="max_guests">Max Guests</label>
+              <input
+                type="number"
+                id="max_guests"
+                name="max_guests"
+                value={property.max_guests}
+                onChange={onInputChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+          </div>
+          <div className="row">
+          <div className="col-md-6">
+            <div className="form-group mb-2">
+              <label htmlFor="price_per_night">Price Per Night</label>
+              <input
+                type="number"
+                id="price_per_night"
+                name="price_per_night"
+                value={property.price_per_night}
+                onChange={onInputChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group mb-2">
+              <label htmlFor="bedrooms">Bedrooms</label>
+              <input
+                type="number"
+                id="bedrooms"
+                name="bedrooms"
+                value={property.bedrooms}
+                onChange={onInputChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+          </div>
+          <div className="row">
+          <div className="col-md-6">
+            <div className="form-group mb-2">
+              <label htmlFor="beds">Beds</label>
+              <input
+                type="number"
+                id="beds"
+                name="beds"
+                value={property.beds}
+                onChange={onInputChange}
+                className="form-control"
+              />
+            </div>
+          </div>
+        <div className="col-md-6">
         <div className="form-group mb-2">
-          <label htmlFor="beds">Beds</label>
+          <label htmlFor="baths">Baths</label>
           <input
             type="number"
-            id="beds"
-            name="beds"
-            value={property.beds}
+            id="baths"
+            name="baths"
+            value={property.baths}
             onChange={onInputChange}
             className="form-control"
           />
         </div>
-      </div>
-    <div className="col-md-6">
-    <div className="form-group mb-2">
-      <label htmlFor="baths">Baths</label>
-      <input
-        type="number"
-        id="baths"
-        name="baths"
-        value={property.baths}
-        onChange={onInputChange}
-        className="form-control"
-      />
-    </div>
-    </div>
-    </div>
-    <div className="form-group mb-2">
-      <label htmlFor="images">Images</label>
-      <input
-        type="file"
-        id="image-input"
-        name="image-input"
-        accept="image/*"
-        onChange={onInputChange}
-        className="form-control"
-      />
-      <small className="form-text text-muted">Choose one or more images to upload.</small>
-    </div>
+        </div>
+        </div>
+        <div className="form-group mb-2">
+          <label htmlFor="images">Images</label>
+          <input
+            type="file"
+            id="image-input"
+            name="image-input"
+            accept="image/*"
+            onChange={onInputChange}
+            className="form-control"
+          />
+          <small className="form-text text-muted">Choose one or more images to upload.</small>
+        </div>
         <button type="submit" className="btn btn-primary mt-2 mb-2">Save</button>
         <button type="button" onClick={onCancel} className="btn btn-secondary mt-2 mb-2">Cancel</button>
       </form>
